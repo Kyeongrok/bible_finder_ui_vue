bf5489d067f32d39a6b3d6da555cf03d80f0fca7
Kyeongrok <oceanfog1@gmail.com>
2022-03-30 05:20:24 +0900

    난수 생성 기능 등 추가

--- src/views/Seats.vue
+++ src/views/Seats.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <p :key="i" v-for="(name, i) in names">{{name}}</p>
-    <input type="text">
+    <div>인원 수:{{names.length}} 시작주:<input type="text" v-model="weeksFrom"> 끝주:<input type="text" v-model="weeksTo"></div>
+    <span :key="i" v-for="(name, i) in names">{{name}}</span>
+    <br/>
+    <input type="text" v-model="currentName">
     <button @click="addNames">+</button>
-    <button>만들기</button>
+    <br/>
+    <button @click="run">만들기</button>
+    <table>
+      <tr :key="i" v-for="(result, i) in results">
+        <td>{{i+1}}</td>
+        <td>{{result}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -14,12 +23,34 @@ export default {
     return{
       input: '롬5:1',
       names:[],
+      results:[],
+      weeksFrom:1,
+      weeksTo:40,
+      currentName:"",
       message: '이곳에 결과가 나옵니다.'
     }
   },
   methods: {
     addNames: function () {
-      this.names.push("김경록")
+      if (this.currentName != ""){
+        this.names.push(this.currentName)
+        this.currentName = ""
+      } else {
+        alert("이름을 입력하세요.")
+      }
+    },
+    run: function () {
+      this.results.push(this.rndRange(this.names.length))
+    },
+    rndRange: (totalIndex) => {
+      let randomIndexArray = []
+      while (randomIndexArray.length < totalIndex){
+        let randomNum = Math.floor(Math.random() * totalIndex)
+        if (randomIndexArray.indexOf(randomNum) === -1) {
+          randomIndexArray.push(randomNum)
+        }
+      }
+      return randomIndexArray
     }
   }
 }
